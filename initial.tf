efae2a8a3b704c0e1eafbf10218c535633df5112
saipranayteja <pranayteja.ch@algonox.com>
2023-06-27 20:08:36 +0530

    sdcfv

--- initial.tf
+++ initial.tf
@@ -1,18 +1,18 @@
-resource "jenkins_folder" "all_jobs" {
+/* resource "jenkins_folder" "all_jobs" {
   name        = "all_jobs"
   description = "Folder for creation of all jobs"
-}
+} */
 
 
 
 resource "jenkins_job" "initial-jobs" {
-    depends_on = [ jenkins_folder.all_jobs ]
+   #depends_on = [ jenkins_folder.all_jobs ]
     name     = "All-Jobs-setup"
-    folder="/job/all_jobs"
+    #folder="/job/all_jobs"
     template = templatefile("${path.module}/initial.xml", {
         all_repo=var.all_repo
         name     = "All-Jobs-setup"
-        filename="All-jobs-setup"
+        #filename="All-jobs-setup"
         description = "Roboshop Pipeline for Infra"
     })
 
