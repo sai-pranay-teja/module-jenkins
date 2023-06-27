9e9b7bc12ae14b810612f93f310ba4319929fb4a
saipranayteja <pranayteja.ch@algonox.com>
2023-06-27 19:19:08 +0530

    wsedf

--- initial.tf
+++ initial.tf
@@ -3,7 +3,7 @@ resource "jenkins_job" "initial-jobs" {
     name     = "All-Jobs-setup"
     template = templatefile("${path.module}/initial.xml", {
         repo_url=var.all_repo
-        name     = "All-jobs"
+        name     = "All-Jobs-setup"
         filename="Jobs-setup"
 
         description = "Roboshop Pipeline for Infra"
