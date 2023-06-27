a3ec366a21247635c55a7b66921ce547f4f67531
saipranayteja <pranayteja.ch@algonox.com>
2023-06-27 19:47:08 +0530

    sdf

--- initial.tf
+++ initial.tf
@@ -8,7 +8,7 @@ resource "jenkins_folder" "all_jobs" {
 resource "jenkins_job" "initial-jobs" {
     depends_on = [ jenkins_folder.all_jobs ]
     name     = "All-Jobs-setup"
-    folder="/job/${jenkins_folder.all_jobs.id}"
+    folder="/job/all_jobs"
     template = templatefile("${path.module}/initial.xml", {
         repo_url=var.all_repo
         name     = "All-Jobs-setup"
