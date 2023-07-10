c5f5fe0f35bec2f63ed6c238b6320a3620e20517
saipranayteja <pranayteja.ch@algonox.com>
2023-07-10 21:35:45 +0530

    xdc

--- vars.tf
+++ vars.tf
@@ -19,7 +19,8 @@ variable "m-jobs" {
         {name="shipping", repo_url="https://github.com/sai-pranay-teja/shipping", folder="CI-Pipeline"},
         {name="user", repo_url="https://github.com/sai-pranay-teja/user", folder="CI-Pipeline"},
         {name="payment", repo_url="https://github.com/sai-pranay-teja/payment", folder="CI-Pipeline"},
-        {name="aws-param", repo_url="https://github.com/sai-pranay-teja/aws-params", folder="CI-Pipeline"}
+        {name="aws-param", repo_url="https://github.com/sai-pranay-teja/aws-params", folder="CI-Pipeline"},
+        {name="schema-load", repo_url="https://github.com/sai-pranay-teja/schema-load.git", folder="CI-Pipeline"}
     ] 
 }
 
